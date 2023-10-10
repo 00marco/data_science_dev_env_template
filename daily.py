3eed9854f3a390020421a477033e3d923015a206
Marco Pulido <marcopulido00@gmail.com>
2023-10-10 18:36:30 +0800

    removed from_dockerfile image call after getting some bug -- scheduled job now working after the fix

--- daily.py
+++ daily.py
@@ -1,16 +1,26 @@
 # heavy.py
 import modal
-import logger
+import logging
 
-docker_image = modal.Image.from_dockerfile("socmed_ml_pipeline.Dockerfile")
-stub = modal.Stub()
+logging.basicConfig(
+    level=logging.DEBUG,              # Set the logging level to DEBUG
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+
+# docker_image = modal.Image.from_dockerfile("socmed_ml_pipeline.Dockerfile")
+docker_image = modal.Image.debian_slim().pip_install("pandas", "numpy")
+stub = modal.Stub("socmed_ml_pipeline")
 
 # runs at 8 am (UTC) every Monday
 @stub.function(image=docker_image, schedule=modal.Cron("0 8 * * 1"))
 def perform_heavy_computation():
-    logger.log("run script")
+    logging.debug("run script")
 
 
 @stub.local_entrypoint()
 def main():
     perform_heavy_computation.remote()
+
+if __name__ == "__main__":
+    perform_heavy_computation()
